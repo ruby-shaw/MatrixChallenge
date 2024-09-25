44d7e9b17ea56116e1ac99ab8b9f96a031eafb8e
ruby.shaw <ruby.shaw@MU-C1-17.mullen.pvt.k12.co.us>
2024-09-25 11:49:01 -0600

    Initial Commit

---
+++ Matrix Challenge/src/MatrixChallenge.java
@@ -0,0 +1,16 @@
+
+public class MatrixChallenge
+	{
+
+		public static void main(String[] args)
+			{
+				
+				for(int i =0; i < Numbers.myArray.length; i++)
+					{
+						
+						String [] nums =Integer.toString(Numbers.myArray);
+					}
+
+			}
+
+	}
