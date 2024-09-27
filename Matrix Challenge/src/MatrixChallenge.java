79a7c73ebec14d9bd4d8333ff4de8002ccfbf55b
ruby.shaw <ruby.shaw@MU-C1-17.mullen.pvt.k12.co.us>
2024-09-27 12:01:14 -0600

    Final Commit

--- Matrix Challenge/src/MatrixChallenge.java
+++ Matrix Challenge/src/MatrixChallenge.java
@@ -5,11 +5,29 @@ public class MatrixChallenge
 		public static void main(String[] args)
 			{
 				
-				for(int i =0; i < Numbers.myArray.length; i++)
+				int [ ] [ ] myArray = {{3, 5, 9, 2, -1, 8, 13}, {-2, 5, 1, 11, 8, 2, 0}, {9, 1, -3, 4, 8, 4, 8}, {6, 0, 0, -1, 4, 12, 7}, {2, 6, -1, 5, 3, 10, 1}, {2, 5, 1, 2, 9, 2, 3}, {-3, 1, 1, 4, 11, 12, 4}};
+				int maxNum = Integer.MIN_VALUE;
+				int minNum = Integer.MAX_VALUE;
+				double sum =0.0;
+				
+				for(int row =0; row < myArray.length; row++)
 					{
-						
-						String [] nums =Integer.toString(Numbers.myArray);
+						for (int col =0; col < myArray[0].length; col++)
+							{
+								 sum = sum + myArray[row][col];
+								if(myArray[row][col] > maxNum)
+									{
+										maxNum = myArray[row][col];
+									}
+								else if (myArray[row][col] < minNum)
+									{
+										minNum = myArray[row][col];
+									}
+							}
 					}
+				double mean = sum / (myArray.length * myArray[0].length);
+				System.out.println("The largest number is " + maxNum + ", the smallest number is " + minNum+ ", and the mean is " + mean);
+				
 
 			}
 
